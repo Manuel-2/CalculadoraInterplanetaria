030b64e9135ca15027e74f15a0cb783295d238fd
Manuel <25lopezm25@gmail.com>
2021-06-09 11:56:40 -0600

    initial commit with the complete project :D

---
+++ code.js
@@ -0,0 +1,29 @@
+// 0 for eathr, 1 mars, 2 moon, 3 jupiter
+const GRAVITYS = [9.8, 3.7, 1.62, 24.7];
+
+function getData() {
+    let weight = document.getElementById("weightInput").value;
+    let planet = document.getElementById("planetsSelection").value;
+
+    let planetGravity;
+
+    switch (planet) {
+        case "mars":
+            planetGravity = GRAVITYS[1];
+            break;
+        case "moon":
+            planetGravity = GRAVITYS[2];
+            break;
+        case "jupiter":
+            planetGravity = GRAVITYS[3];
+            break;
+    }
+
+    let result = (planetGravity * weight) / GRAVITYS[0];
+    printData(result);
+}
+
+function printData(value) {
+    let outputTextElement = document.getElementById("resultOutput");
+    outputTextElement.innerText = value;
+}
